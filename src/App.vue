92e9fa236afab7eaae171fbc1eececf49220ce96
LE GOFF Vincent <g_n_s@hotmail.fr>
2019-09-05 00:10:35 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app" class="container">
+    <div class="section">
+      <div class="has-text-centered">Hello</div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component({
+  components: {}
+})
+export default class App extends Vue {}
+</script>
---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import Vue from "vue";
+// import "bulma/css/bulma.css";
+// import "@fortawesome/fontawesome-free/css/all.css";
+import App from "./App.vue";
+import store from "./store";
+
+Vue.config.productionTip = false;
+
+new Vue({
+  store,
+  render: h => h(App)
+}).$mount("#app");
