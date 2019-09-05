9b8e5528cd78c2f8eb1cb74f99aeb33af49bebec
LE GOFF Vincent <g_n_s@hotmail.fr>
2019-09-05 17:52:23 +0200

    addition

---
+++ src/keysets/gmk.ts
@@ -0,0 +1,38 @@
+export default [
+  {
+    name: "bento",
+    designer: "biip",
+    colors: {
+      base: {
+        background: "#fff3ef",
+        color: "#3e728f"
+      },
+      novelty: {
+        background: "#fb7279",
+        color: "#3e728f"
+      },
+      accent: {
+        background: "#3e728f",
+        color: "#fb7279"
+      }
+    }
+  },
+  {
+    name:"carbon",
+    designer:"tomb3ry",
+    colors: {
+      base: {
+        background: "#cec1a9",
+        color: "#464746"
+      },
+      novelty: {
+        background: "#464746",
+        color: "#de6722"
+      },
+      accent: {
+        background: "#de6722",
+        color: "#464746"
+      }
+    }
+  }
+];
--- src/store.ts
+++ src/store.ts
@@ -1,12 +1,18 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import gmk from "./keysets/gmk";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    gb: {}
   },
   mutations: {},
-  actions: {}
+  actions: {
+    genKeyset(_, name) {
+      const s = gmk.find(e => {
+        return e.name === name;
+      });
+    }
+  }
 });
