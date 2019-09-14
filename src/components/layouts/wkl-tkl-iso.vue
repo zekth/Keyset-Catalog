3e9c044b6638cc46e8697182ec0bf226a92128ac
LE GOFF Vincent <g_n_s@hotmail.fr>
2019-09-14 12:51:26 +0200

    fix typo iso

--- src/components/layouts/wkl-tkl-iso.vue
+++ src/components/layouts/wkl-tkl-iso.vue
@@ -515,7 +515,7 @@
       <U1 x="734" :data="getKeyData('KC_L', 'base', 'L')" />
       <U1 x="810" :data="getKeyData('KC_M', 'base', 'M')" />
       <U1 x="886" :data="getKeyData('KC_QUOT', 'base', '%', 'ù')" />
-      <U1 x="962" :data="getKeyData('KC_QUOT', 'base', '%', 'ù')" />
+      <U1 x="962" :data="getKeyData('KC_QUOT', 'base', 'µ', '*')" />
       <!-- <U225
         x="963"
         :data="getKeyData('KC_ENT', 'accent', 'Enter')"
