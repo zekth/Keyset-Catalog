e9bbcf763d74142bd11a3499cff2c44896c4fb3d
LE GOFF Vincent <g_n_s@hotmail.fr>
2019-09-19 09:08:05 -0700

    component splitting

--- src/App.vue
+++ src/App.vue
@@ -164,30 +164,8 @@
           <ToggleButton @change="toggleDarkMode" v-model="darkMode" />
         </div>
       </div>
-      <div class="row marketing">
-        <div class="col-lg-6">
-          <h4>Just a database</h4>
-          <p>Just here to have a list of all the available keysets</p>
-          <h4>Why there is no novelties?</h4>
-          <p>Because i do not own those designs neither the SVG files.</p>
-          <h4>OMG UI sucks?!</h4>
-          <p>I know, but it works; right?</p>
-        </div>
-        <div class="col-lg-6">
-          <h4>Searching</h4>
-          <p>Provide a way find the best colormatch</p>
-          <h4>Missing keysets?</h4>
-          <p>Just submit a pull Request</p>
-          <h4>Disclaimer</h4>
-          <p>
-            Some colors may be off because i do not own all the keysets listed.
-            Also the colors are mostly from the renders and no the final
-            product, so some colors may differ.
-          </p>
-        </div>
-      </div>
+      <appDescription />
     </div>
-
     <appFooter />
   </div>
 </template>
@@ -207,11 +185,13 @@ import lubrigante from '@/components/layouts/lubrigante.vue';
 import wklTkl from '@/components/layouts/wkl-tkl.vue';
 import wklTklIso from '@/components/layouts/wkl-tkl-iso.vue';
 import appHeader from '@/components/header.vue';
+import appDescription from '@/components/description.vue';
 import appFooter from '@/components/footer.vue';
 import split60 from '@/components/layouts/60SplitBckSp.vue';
 @Component({
   components: {
     VueSlider,
+    appDescription,
     appHeader,
     fullSizeAnsi,
     appFooter,
---
+++ src/components/description.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="row marketing">
+    <div class="col-lg-6">
+      <h4>Just a database</h4>
+      <p>Just here to have a list of all the available keysets</p>
+      <h4>Why there is no novelties?</h4>
+      <p>Because i do not own those designs neither the SVG files.</p>
+      <h4>OMG UI sucks?!</h4>
+      <p>I know, but it works; right?</p>
+    </div>
+    <div class="col-lg-6">
+      <h4>Searching</h4>
+      <p>Provide a way find the best colormatch</p>
+      <h4>Missing keysets?</h4>
+      <p>Just submit a pull Request</p>
+      <h4>Disclaimer</h4>
+      <p>
+        Some colors may be off because i do not own all the keysets listed. Also
+        the colors are mostly from the renders and no the final product, so some
+        colors may differ.
+      </p>
+    </div>
+  </div>
+</template>
